c4041ace1391913dfcac59f0c76304f863cfcf1b
Ashen Dunusinghe <60811307+Avdunusinghe@users.noreply.github.com>
2021-09-19 00:15:38 +0530

    User Model Class Implemented

--- app/src/main/java/com/example/model/User.java
+++ app/src/main/java/com/example/model/User.java
@@ -1,4 +1,61 @@
 package com.example.model;
 
 public class User {
+
+    private int id;
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private String password;
+
+    public User() {
+    }
+
+    public User(int id, String name, String email, String phoneNumber, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }

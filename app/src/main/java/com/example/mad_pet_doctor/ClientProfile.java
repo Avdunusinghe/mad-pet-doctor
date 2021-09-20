4a1886e5e39631c9044d9055b8d4e3c8601261ba
Ashen Dunusinghe <60811307+Avdunusinghe@users.noreply.github.com>
2021-09-20 21:34:32 +0530

    client Profile partilay Implemeted v2

--- app/src/main/java/com/example/mad_pet_doctor/ClientProfile.java
+++ app/src/main/java/com/example/mad_pet_doctor/ClientProfile.java
@@ -4,6 +4,9 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -17,15 +20,28 @@ public class ClientProfile extends AppCompatActivity {
 
     String userId;
 
+    EditText name,phoneNumber;
 
+    TextView email;
+
+    DatabaseReference dbRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client_profile);
 
+        //initializing
+        name = findViewById(R.id.editProfile_nameEditText);
+
+        email = findViewById(R.id.editProfile_emailEditText);
+
+        phoneNumber = findViewById(R.id.editProfile_movileNumberEditText);
+
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         userId = user.getUid();
+
+        getUserDetails();
 
     }
 
@@ -39,7 +55,11 @@ public class ClientProfile extends AppCompatActivity {
 
                 if(snapshot.hasChildren()){
 
+                    name.setText(snapshot.child("name").getValue().toString());
 
+                    email.setText(snapshot.child("email").getValue().toString());
+
+                    phoneNumber.setText(snapshot.child("phoneNumber").getValue().toString());
                 }
             }
 
@@ -49,5 +69,51 @@ public class ClientProfile extends AppCompatActivity {
             }
         });
     }
+
+    public void updateUserProfile(){
+
+        if(validateMobileNumber()){
+
+            dbRef = FirebaseDatabase.getInstance().getReference().child("User").child(userId);
+
+            dbRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                    dbRef.child("name").setValue(name.getText().toString().trim());
+
+                    dbRef.child("phoneNumber").setValue(name.getText().toString().trim());
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+        }
+
+    }
+
+
+    public boolean validateMobileNumber()
+    {
+        String userInputPhoneNumber = phoneNumber.getText().toString();
+
+        if(userInputPhoneNumber.length() == 10)
+        {
+            return true;
+        }
+        else {
+
+            Toast.makeText(getApplicationContext(),
+                    "Please enter valid phone number",
+                    Toast.LENGTH_SHORT)
+                    .show();
+
+            return false;
+        }
+
+    }
+
 
 }

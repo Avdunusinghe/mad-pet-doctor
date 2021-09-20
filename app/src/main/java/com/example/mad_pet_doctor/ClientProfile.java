c0f08f45852a769745524b8110822b211153f8d3
Ashen Dunusinghe <60811307+Avdunusinghe@users.noreply.github.com>
2021-09-20 20:31:32 +0530

    client Profile partilay Implemeted

--- app/src/main/java/com/example/mad_pet_doctor/ClientProfile.java
+++ app/src/main/java/com/example/mad_pet_doctor/ClientProfile.java
@@ -1,14 +1,53 @@
 package com.example.mad_pet_doctor;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class ClientProfile extends AppCompatActivity {
+
+    String userId;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client_profile);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        userId = user.getUid();
+
     }
+
+    public void getUserDetails(){
+
+        DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference().child("User").child((userId));
+
+        dbRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                if(snapshot.hasChildren()){
+
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
 }

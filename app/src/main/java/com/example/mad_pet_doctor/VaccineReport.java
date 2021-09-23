4199b24b260572ae24789256d67cc8760db73ddf
RanasingheTKSA <sachin.ranasinghe1998.13@gmail.com>
2021-09-23 16:30:17 +0530

    Vaccine Report is done

--- app/src/main/java/com/example/mad_pet_doctor/VaccineReport.java
+++ app/src/main/java/com/example/mad_pet_doctor/VaccineReport.java
@@ -1,63 +1,59 @@
 package com.example.mad_pet_doctor;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.RadioGroup;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
+import com.example.list_view.UserList;
+import com.example.list_view.VaccineReportList;
+import com.example.model.User;
 import com.example.model.VaccineDetailsModal;
 import com.google.android.material.chip.Chip;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class VaccineReport extends ArrayAdapter<VaccineDetailsModal> {
+public class VaccineReport extends AppCompatActivity {
 
-    private Activity context;
-    List<VaccineDetailsModal> vaccineDetailsModals;
+    private TextView VRHeading;
+    private ImageView VRImage;
+    private ImageButton VRAddButton;
+    private Button DoneButton;
+    private String VaccineReportID;
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
+    private VaccineDetailsModal vaccineDetailsModal;
+    private Chip HomeChip1, HomeChip2;
 
-    public VaccineReport(Activity context, List<VaccineDetailsModal> vaccineDetailsModals) {
-        super(context, R.layout.vaccine_report_list, vaccineDetailsModals);
-        this.context = context;
-        this.vaccineDetailsModals = vaccineDetailsModals;
-    }
+    ListView vaccineReportListView;
+    List<VaccineDetailsModal> vaccineReportList;
+    DatabaseReference db;
 
     @Override
-    public View getView (int position, View convertView, ViewGroup parent){
-        LayoutInflater inflater = context.getLayoutInflater();
-        View listViewItem = inflater.inflate(R.layout.vaccine_report_list_details, null, true);
-        TextView textViewVRName = (TextView) listViewItem.findViewById(R.id.VR_name_list_textview);
-        TextView textViewVRDate = (TextView) listViewItem.findViewById(R.id.VR_date_list_textview);
-        TextView textViewVRPrice = (TextView) listViewItem.findViewById(R.id.VR_price_list_textview);
-
-
-        VaccineDetailsModal vaccineDetailsModal = vaccineDetailsModals.get(position);
-        textViewVRName.setText(vaccineDetailsModal.getVaccine_Name_Edt());
-        textViewVRDate.setText(vaccineDetailsModal.getVaccine_Date_Edt());
-        textViewVRPrice.setText(vaccineDetailsModal.getVaccine_Price_Edt());
-
-        return listViewItem;
-    }
-
-}
-    /*@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.vaccine_report);
+        setContentView(R.layout.vaccine_report_list);
+
+        Intent intent = getIntent();
+        vaccineReportList = new ArrayList<>();
+        db = FirebaseDatabase.getInstance().getReference("VaccineDetails");
+        vaccineReportListView = (ListView)findViewById(R.id.VRlistview);
 
         //FirebaseDatabase Get Instance
         firebaseDatabase = FirebaseDatabase.getInstance();
@@ -66,11 +62,6 @@ public class VaccineReport extends ArrayAdapter<VaccineDetailsModal> {
         VRHeading =  findViewById(R.id.vaccineReportHeading);
         VRImage = findViewById(R.id.vaccineReportImage);
         VRAddButton = findViewById(R.id.VaccineReportAddButton);
-        VRTable = findViewById(R.id.Table2);
-        Column1Data = findViewById(R.id.tablecoloum5);
-        Column2Data = findViewById(R.id.tablecoloum6);
-        Column3Data = findViewById(R.id.tablecoloum7);
-        Column4Data = findViewById(R.id.tablecoloum8);
         DoneButton = findViewById(R.id.savebutton);
         HomeChip1 = findViewById(R.id.chip12);
         HomeChip2 = findViewById(R.id.chip15);
@@ -101,5 +92,35 @@ public class VaccineReport extends ArrayAdapter<VaccineDetailsModal> {
                 startActivity(new Intent(VaccineReport.this, MainActivity.class));
             }
         });
+    }
 
-    }*/
+    @Override
+    public void onStart(){
+        super.onStart();
+
+        db.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                vaccineReportList.clear();
+
+                for(DataSnapshot VaccineDetailDataSnapshot:snapshot.getChildren()){
+
+                    VaccineDetailsModal vaccineDetailsModals = VaccineDetailDataSnapshot.getValue(VaccineDetailsModal.class);
+
+                    vaccineReportList.add(vaccineDetailsModals);
+                }
+
+                VaccineReportList vaccineReportAdapter = new VaccineReportList(VaccineReport.this,vaccineReportList);
+                vaccineReportListView.setAdapter(vaccineReportAdapter);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    }
+}
